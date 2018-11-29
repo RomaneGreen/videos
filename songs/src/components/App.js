3502550a41ddef12a9719e2c03c89b1b8a1f2462
Romane Green <Romane71193@gmail.com>
2018-11-29 16:49:50 -0500

    able to select song

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -3,9 +3,14 @@ import { selectSong } from '../actions'
 import SongList from './SongList'
 const App = () => {
     return (
-        <div>
+        <div className="ui container grid">
+        <div className = "ui row">
+        <div className="column eight wide">
+
         <SongList />
             
+        </div>
+        </div>
         </div>
     );
 };
